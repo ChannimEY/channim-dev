58a058bfe15d804a12f388c5af64377cc9266033
ChannimEY <channimey1@gmail.com>
2026-05-25 01:53:44 +0700

    fix error

--- hooks/use-toast.ts
+++ hooks/use-toast.ts
@@ -3,7 +3,16 @@
 // Inspired by react-hot-toast library
 import * as React from 'react'
 
-import type { ToastActionElement, ToastProps } from '@/components/ui/toast'
+interface ToastActionElement {
+  action: string
+  altText: string
+  onClick: () => void
+}
+
+interface ToastProps {
+  open?: boolean
+  onOpenChange?: (open: boolean) => void
+}
 
 const TOAST_LIMIT = 1
 const TOAST_REMOVE_DELAY = 1000000
